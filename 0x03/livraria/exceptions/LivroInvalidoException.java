0bfeb4c9a2240d46086fce388286ccf7813bfcbf
João Domingues <jmmarao@ciandt.com>
2022-06-24 14:20:13 -0300

    [TASK-1] Bookstore: updated packages

--- 0x03/livraria/exceptions/LivroInvalidoException.java
+++ 0x03/livraria/exceptions/LivroInvalidoException.java
@@ -1,3 +1,5 @@
+package week03.project13.livraria.exceptions;
+
 public class LivroInvalidoException extends Exception {
     public LivroInvalidoException(String message) {
         super(message);
