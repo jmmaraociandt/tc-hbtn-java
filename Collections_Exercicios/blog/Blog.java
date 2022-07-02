49cfa8fbb973e28519f198fb9eee0ce8b415accf
João Domingues <jmmarao@ciandt.com>
2022-07-01 23:02:20 -0300

    [TASK-0] Blog posts

---
+++ Collections_Exercicios/blog/Blog.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Blog {
+    List<Post> posts;
+
+    public Blog() {
+        this.posts = new ArrayList<>();
+    }
+
+    public void adicionarPostagem(Post post) {
+        this.posts.add(post);
+    }
+
+    public Set<String> obterTodosAutores() {
+        Set<String> authors = new TreeSet<>();
+        Iterator<Post> postIterator = this.posts.iterator();
+
+        while (postIterator.hasNext()) {
+            Post post = postIterator.next();
+            authors.add(post.getAutor());
+        }
+        return authors;
+    }
+
+    public Map<String, Integer> obterContagemPorCategoria() {
+        int count = 1;
+        Map<String, Integer> subjects = new TreeMap<>();
+        Iterator<Post> postIterator = this.posts.iterator();
+
+        while (postIterator.hasNext()) {
+            Post post = postIterator.next();
+
+            if (subjects.containsKey(post.getCategoria())) {
+                count = subjects.get(post.getCategoria());
+                count++;
+            }
+
+            subjects.put(post.getCategoria(), count);
+        }
+        return subjects;
+    }
+}
