3c966f744a8a6168007a4f258256d01644e68147
João Domingues <jmmarao@ciandt.com>
2022-07-02 19:52:06 -0300

    [TASK-0] Phone number: refactored arraylist name

--- Enum/celular/Celular.java
+++ Enum/celular/Celular.java
@@ -1,16 +1,15 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class Celular {
-    private ArrayList<Contato> contacts;
+    private ArrayList<Contato> contatos;
 
     public Celular() {
-        this.contacts = new ArrayList<Contato>();
+        this.contatos = new ArrayList<>();
     }
 
     public int obterPosicaoContato(String name) {
-        for (int i = 0; i < contacts.size(); i++) {
-            if (contacts.get(i).getName().equals(name))
+        for (int i = 0; i < contatos.size(); i++) {
+            if (contatos.get(i).getName().equals(name))
                 return i;
         }
         return -1;
@@ -19,7 +18,7 @@ public class Celular {
     public void adicionarContato(Contato contactToAdd) {
         if (obterPosicaoContato(contactToAdd.getName()) != -1)
             throw new IllegalArgumentException("Nao foi possivel adicionar contato. Contato jah existente com esse nome");
-        this.contacts.add(contactToAdd);
+        this.contatos.add(contactToAdd);
     }
 
     public void atualizarContato(Contato contatoAntigo, Contato novoContato) {
@@ -28,17 +27,17 @@ public class Celular {
         if (obterPosicaoContato(novoContato.getName()) != -1 &&
                 !contatoAntigo.getName().equals(novoContato.getName()))
             throw new IllegalArgumentException("Nao foi possivel modificar contato. Contato jah existente com esse nome");
-        this.contacts.set(obterPosicaoContato(contatoAntigo.getName()), novoContato);
+        this.contatos.set(obterPosicaoContato(contatoAntigo.getName()), novoContato);
     }
 
     public void removerContato (Contato contactToRemove) {
         if (obterPosicaoContato(contactToRemove.getName()) == -1)
             throw new IllegalArgumentException("Nao foi possivel remover contato. Contato nao existe");
-        this.contacts.remove(obterPosicaoContato(contactToRemove.getName()));
+        this.contatos.remove(obterPosicaoContato(contactToRemove.getName()));
     }
 
     public void listarContatos() {
-        for (Contato contact : contacts) {
+        for (Contato contact : contatos) {
             System.out.printf("%s -> %s (%s)%n", contact.getName(), contact.getPhone(), contact.getNumberType());
         }
     }
