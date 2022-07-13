eec69892f97c668408f31d655ca7bd438aa5671f
João Domingues <jmmarao@ciandt.com>
2022-07-13 18:38:22 -0300

    [TASK-3] Average salary by position

---
+++ Streams_Collector_Optional_Exercicios/media_salario_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> people) {
+
+        return people.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo,
+                            () -> new TreeMap<>(Comparator.reverseOrder()),
+                            Collectors.toCollection(TreeSet::new)));
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> people) {
+        return people.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo,
+                            Collectors.counting()));
+    }
+
+    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> people) {
+        return people.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo,
+                            Collectors.groupingBy(Pessoa::getIdade,
+                                    Collectors.counting())));
+    }
+
+    public static Map<String, Double> obterMediaSalarioPorCargo(List<Pessoa> people) {
+        return people.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo,
+                        Collectors.averagingDouble(Pessoa::getSalario)));
+    }
+}
