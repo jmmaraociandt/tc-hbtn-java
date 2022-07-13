3a343fca6ad1416890d0011782779a82a3f92f12
João Domingues <jmmarao@ciandt.com>
2022-07-13 10:50:01 -0300

    [TASK-4] Eletronics 15% off

---
+++ Streams_Collectors_Optional/desconto_eletronicos/Consulta.java
@@ -0,0 +1,39 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(bookOnly -> bookOnly.getCategoria() == CategoriaProduto.LIVRO)
+                .collect(Collectors.toList());
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> products) {
+        return products.stream()
+                .sorted(Comparator.comparing(Produto::getPreco).reversed())
+                .findFirst().get();
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> products, double minValuePrice) {
+        return products.stream()
+                .filter(product -> product.getPreco() >= minValuePrice)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> orders) {
+        return orders.stream()
+                .filter(product -> product.getProdutos().stream().anyMatch(eletronicsOnly -> eletronicsOnly.getCategoria() == CategoriaProduto.ELETRONICO))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Produto> aplicar15PorcentoDescontoEletronicos(List<Produto> products) {
+        return products.stream()
+                .map(eletronicProduct -> {
+                    if (eletronicProduct.getCategoria() == CategoriaProduto.ELETRONICO)
+                        eletronicProduct.setPreco(0.85*eletronicProduct.getPreco());
+                    return eletronicProduct;
+                })
+                .collect(Collectors.toList());
+    }
+}
---
+++ Streams_Collectors_Optional/desconto_eletronicos/Produto.java
@@ -0,0 +1,30 @@
+public class Produto {
+    private int codigo;
+    private String nome;
+    private CategoriaProduto categoria;
+    private double preco;
+
+    public Produto(int codigo, String nome, CategoriaProduto categoria, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+    }
+
+    public CategoriaProduto getCategoria() {
+        return categoria;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s R$ %.2f", codigo, nome, categoria, preco);
+    }
+}
